ee20779ea30082c07e4f494deae270104a053fd6
Aulia Syarif Aziz <as.sektim@gmail.com>
2023-08-14 20:24:22 +0700

    Initial commit

---
+++ migrations/1689575193214_create-table-albums.js
@@ -0,0 +1,30 @@
+// eslint-disable-next-line no-unused-vars
+exports.up = (pgm) => {
+  /**
+   * 1. @TODO (DONE)
+   * Buatlah table albums dengan `pgm.createTable` dengan kolom:
+   *  - id: primary key, tipe data string (varchar), dan tidak boleh null
+   *  - name: tipe data string (varchar), dan tidak boleh null
+   *  - year: tipe data integer, dan tidak boleh null
+   *
+   *  Catatan: referensi modul Dicoding: https://www.dicoding.com/academies/271/tutorials/17468
+   */
+  pgm.createTable('albums', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'VARCHAR(50)',
+      notNULL: true,
+    },
+    year: {
+      type: 'INTEGER',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('albums');
+};
---
+++ src/api/songs/index.js
@@ -0,0 +1,20 @@
+const SongsHandler = require('./handler');
+const SongsValidator = require('./validator');
+const routes = require('./routes');
+
+const songs = {
+  name: 'songs',
+  register: async (server, { songsService }) => {
+    const handler = new SongsHandler(SongsValidator, songsService);
+
+    /**
+     * 3. @TODO (DONE)
+     * Daftarkan `routes(handler)` ke server melalui `server.route()`
+     *
+     * referensi: https://www.dicoding.com/academies/271/tutorials/14432
+     */
+    server.route(routes(handler));
+  },
+};
+
+module.exports = songs;
